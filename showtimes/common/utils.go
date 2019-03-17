a73067f8af8fb179f693caa8e358a04d654059f8
Manuel Morejón <manuel.morejon.85@gmail.com>
2019-03-17 18:53:54 +0100

    Load configuration from environment values in showtimes api.

--- showtimes/common/utils.go
+++ showtimes/common/utils.go
@@ -54,6 +54,42 @@ func initConfig() {
 	if err != nil {
 		log.Fatalf("[logAppConfig]: %s\n", err)
 	}
+
+// Load configuration from environment values if exists
+loadConfigFromEnvironment(&AppConfig)
+}
+
+// Load configuration from environment
+func loadConfigFromEnvironment(appConfig *configuration)  {
+	server, ok := os.LookupEnv("CINEMA_SHOWTIMES")
+	if ok {
+		appConfig.Server = server	
+		log.Printf("[INFO]: Server information loaded from env.")
+	}
+	
+	mongodbHost, ok := os.LookupEnv("MONGODB_HOST")
+	if ok {
+		appConfig.MongoDBHost = mongodbHost	
+		log.Printf("[INFO]: MongoDB host information loaded from env.")
+	}
+	
+	mongodbUser, ok := os.LookupEnv("MONGODB_USER")
+	if ok {
+		appConfig.DBUser = mongodbUser
+		log.Printf("[INFO]: MongoDB user information loaded from env.")
+	}
+	
+	mongodbPwd, ok := os.LookupEnv("MONGODB_PWD")
+	if ok {
+		appConfig.DBPwd = mongodbPwd
+		log.Printf("[INFO]: MongoDB password information loaded from env.")
+	}
+	
+	database, ok := os.LookupEnv("MONGODB_DATABASE")
+	if ok {
+		appConfig.Database = database
+		log.Printf("[INFO]: MongoDB database information loaded from env.")
+	}
 }
 
 // Session holds the mongodb session for database access
