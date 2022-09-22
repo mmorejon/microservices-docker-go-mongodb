04dde7c7b5a7e0ef6b1ab2a6d71446c2537318b8
Brian Adams <service@contrasting.org>
2022-09-22 04:54:47 -0500

    feat: add do DNS

--- digitalocean/dns.tf
+++ digitalocean/dns.tf
@@ -2,7 +2,10 @@ resource "digitalocean_domain" "do-wayofthesys" {
   name       = "do.wayofthesys.com"
 }
 
-resource "digitalocean_domain" "cinema-do-wayofthesys" {
-  name       = "test.do.wayofthesys.com"
-  ip_address = "159.89.244.226"
+resource "digitalocean_record" "test" {
+  domain = digitalocean_domain.do-wayofthesys.id
+  type   = "A"
+  name   = "www"
+  value  = "159.89.244.226"
 }
+
