3974387b1fe2fcd8408b3ebd5c87e67f11d8eb32
Brian Adams <service@contrasting.org>
2022-09-22 07:05:09 -0500

    feat: add k8s.tf

--- digitalocean/k8s.tf
+++ digitalocean/k8s.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "cinema" {
   name   = "cinema"
   region = "nyc3"
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.22.8-do.1"
+  version = "1.24.4-do.0"
 
   node_pool {
     name       = "core"
