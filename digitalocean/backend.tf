be5c84bdad4759ed913750f6d72ebb8f8c62994d
ExperiencedDevOps <service@contrasting.org>
2022-09-21 21:43:52 -0500

    Add DO backend.tf file

---
+++ digitalocean/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    endpoint                    = "nyc3.digitaloceanspaces.com"
+    key                         = "cinema/prod.tfstate"
+    bucket                      = "wayofthesys"
+    region                      = "us-east-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
