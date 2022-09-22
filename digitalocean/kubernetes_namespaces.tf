ceee3dd872f82390649cfb4c567d3c0acd10a7e5
Brian Adams <service@contrasting.org>
2022-09-22 08:55:23 -0500

    feat: Istio via helm

--- digitalocean/kubernetes_namespaces.tf
+++ digitalocean/kubernetes_namespaces.tf
@@ -5,3 +5,11 @@ resource "kubernetes_namespace" "external-dns" {
     name = "external-dns"
   }
 }
+
+resource "kubernetes_namespace" "istio-system" {
+  depends_on = [digitalocean_kubernetes_cluster.cinema]
+  provider   = kubernetes.cinema
+  metadata {
+    name = "istio-system"
+  }
+}
