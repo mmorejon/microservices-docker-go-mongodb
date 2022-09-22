64a32d606d0b6a5a1998d398aecdc27a9bd793cc
Brian Adams <service@contrasting.org>
2022-09-22 06:12:14 -0500

    feat: remove wayofthesys.com to DO

--- digitalocean/dns.tf
+++ digitalocean/dns.tf
@@ -1,7 +1,3 @@
 resource "digitalocean_domain" "do-wayofthesys-org" {
   name       = "wayofthesys.org"
 }
-
-resource "digitalocean_domain" "do-wayofthesys-com" {
-  name       = "wayofthesys.com"
-}
