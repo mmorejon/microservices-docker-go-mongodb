f28ea2fd36128916eb15056c2ee1c69efe86047e
Brian Adams <service@contrasting.org>
2022-09-24 13:03:41 -0500

    Added istio gateway

---
+++ digitalocean/virtualservice.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_manifest" "virtualservice_resource" {
+  provider = kubernetes.cinema
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "VirtualService"
+    "metadata" = {
+      "name" = "argocd"
+    }
+    "spec" = {
+      "gateways" = [
+        "argocd-gateway",
+      ]
+      "hosts" = [
+        "argocd.${var.domain_name[0]}",
+      ]
+      "http" = [
+        {
+          "route" = [
+            {
+              "destination" = {
+                "host" = "argocd-server"
+                "port" = {
+                  "number" = 80
+                }
+              }
+            },
+          ]
+        },
+      ]
+    }
+  }
+}
