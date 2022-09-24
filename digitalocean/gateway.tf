0458a70747fbd68dd2c3b7386e653b8a2a5b0443
Brian Adams <service@contrasting.org>
2022-09-24 12:18:03 -0500

    Added istio gateway

---
+++ digitalocean/gateway.tf
@@ -0,0 +1,43 @@
+resource "kubernetes_manifest" "gateway_resource" {
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "Gateway"
+    "metadata" = {
+      "name" = "argocd-gateway"
+    }
+    "spec" = {
+      "selector" = {
+        "istio" = "ingressgateway"
+      }
+      "servers" = [
+        {
+          "hosts" = [
+            "argo.${var.domain_name[0]}",
+          ]
+          "port" = {
+            "name"     = "https"
+            "number"   = 443
+            "protocol" = "HTTPS"
+          }
+          "tls" = {
+            "credentialName" = "argo-cert"
+            "mode"           = "SIMPLE"
+          }
+        },
+        {
+          "hosts" = [
+            "argo.${var.domain_name[0]}",
+          ]
+          "port" = {
+            "name"     = "http"
+            "number"   = 80
+            "protocol" = "HTTP"
+          }
+          "tls" = {
+            "httpsRedirect" = true
+          }
+        },
+      ]
+    }
+  }
+}
