c07290adf21e54a0bc4785b3334b54840b81cfd9
Brian Adams <service@contrasting.org>
2022-09-25 16:33:03 -0500

    fix: trigger workflow

--- digitalocean/locals.tf
+++ digitalocean/locals.tf
@@ -1,4 +1,5 @@
 locals {
   istio-repo = "https://istio-release.storage.googleapis.com/charts"
   bookinfo-repo = "https://evry-ace.github.io/helm-charts"
+  argocd-repo = "https://charts.bitnami.com/bitnami"
 }
