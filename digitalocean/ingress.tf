95b6c01d0b447678074bcf7906f89254424a9a53
Brian Adams <service@contrasting.org>
2022-09-25 17:21:33 -0500

    fix: add argocd ingress

---
+++ digitalocean/ingress.tf
@@ -0,0 +1,36 @@
+resource "kubernetes_ingress" "argocd_ingress" {
+  metadata {
+    name      = "argocd-ingress"
+    namespace = "argocd"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+
+  spec {
+    backend {
+      service_name = "argocd-argo-cd-server"
+      service_port = 80
+    }
+
+    rule {
+     host = "argocd.wayofthesys.org"
+      http {
+        path {
+          backend {
+            service_name = "argocd-argo-cd-server"
+            service_port = 80
+          }
+
+          path = "/"
+        }
+
+      }
+    }
+  }
+
+  tls {
+    secret_name = "argocd-secret"
+  }
+}
+
