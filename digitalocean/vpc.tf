85dacadf065a14199adb4a646930243a9faefb9b
Brian Adams <service@contrasting.org>
2022-09-25 13:25:31 -0500

    Added istio gateway

--- digitalocean/vpc.tf
+++ digitalocean/vpc.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_vpc" "cinema" {
   name     = "cinema-network"
   region   = "nyc3"
-  ip_range = "10.0.0.0/22" // 2046 max hosts
+  ip_range = "10.1.0.0/22" // 2046 max hosts
 }
 
 
