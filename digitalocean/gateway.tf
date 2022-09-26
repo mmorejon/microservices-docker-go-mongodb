2bd384535f3efbadfb2a9e0a305941ad90bd808e
Brian Adams <service@contrasting.org>
2022-09-26 08:47:06 -0500

    feat: readd istio

--- digitalocean/gateway.tf
+++ digitalocean/gateway.tf
@@ -1,47 +1,31 @@
-/*
 resource "kubernetes_manifest" "argocd-gateway" {
   provider = kubernetes.cinema
   manifest = {
-    "apiVersion" = "networking.istio.io/v1beta1"
-    "kind"       = "Gateway"
-    "metadata" = {
-      "name"       = "argocd-gateway"
-       "namespace" = "kube-argocd"
+  "apiVersion" = "networking.istio.io/v1beta1"
+  "kind" = "Gateway"
+  "metadata" = {
+    "name" = "argocd"
+    "namespace" = "argocd"
+  }
+  "spec" = {
+    "selector" = {
+      "istio" = "ingressgateway"
     }
-    "spec" = {
-      "selector" = {
-        "istio" = "ingressgateway"
-      }
-      "servers" = [
-        {
-          "hosts" = [
-            "argocd.${var.domain_name[0]}",
-          ]
-          "port" = {
-            "name"     = "https"
-            "number"   = 443
-            "protocol" = "HTTPS"
-          }
-          "tls" = {
-            "credentialName" = "argocd-secret"
-            "mode"           = "SIMPLE"
-          }
-        },
-        {
-          "hosts" = [
-            "argocd.${var.domain_name[0]}",
-          ]
-          "port" = {
-            "name"     = "http"
-            "number"   = 80
-            "protocol" = "HTTP"
-          }
-          "tls" = {
-            "httpsRedirect" = true
-          }
-        },
-      ]
-    }
+    "servers" = [
+      {
+        "hosts" = [
+          "argocd.${var.domain_name[0]}",
+        ]
+        "port" = {
+          "name" = "http"
+          "number" = 80
+          "protocol" = "HTTP"
+        }
+        "tls" = {
+          "httpsRedirect" = false
+        }
+      },
+    ]
   }
 }
-*/
+}
--- digitalocean/virtualservice.tf
+++ digitalocean/virtualservice.tf
@@ -1,35 +1,40 @@
-/* 
 resource "kubernetes_manifest" "virtualservice_resource" {
   provider = kubernetes.cinema
   manifest = {
-    "apiVersion" = "networking.istio.io/v1beta1"
-    "kind"       = "VirtualService"
-    "metadata" = {
-      "name" = "argocd"
-      "namespace"  = "kube-argocd"
-    }
-    "spec" = {
-      "gateways" = [
-        "argocd-gateway",
-      ]
-      "hosts" = [
-        "argocd.${var.domain_name[0]}",
-      ]
-      "http" = [
-        {
-          "route" = [
-            {
-              "destination" = {
-                "host" = "argocd-server"
-                "port" = {
-                  "number" = 80
-                }
+  "apiVersion" = "networking.istio.io/v1beta1"
+  "kind" = "VirtualService"
+  "metadata" = {
+    "name" = "argocd"
+    "namespace" = "argocd"
+  }
+  "spec" = {
+    "gateways" = [
+      "argocd",
+    ]
+    "hosts" = [
+      "argocd.${var.domain_name[0]}",
+    ]
+    "http" = [
+      {
+        "match" = [
+          {
+            "uri" = {
+              "prefix" = "/"
+            }
+          },
+        ]
+        "route" = [
+          {
+            "destination" = {
+              "host" = "argocd-argo-cd-server"
+              "port" = {
+                "number" = 80
               }
-            },
-          ]
-        },
-      ]
-    }
+            }
+          },
+        ]
+      },
+    ]
   }
 }
-*/
+}
