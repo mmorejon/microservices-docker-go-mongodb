6771074d1629d448ac23c6820a28ea878c8cfe81
Brian Adams <service@contrasting.org>
2022-09-25 19:41:14 -0500

    fix: add argocd ingress

---
+++ digitalocean/lb.tf
@@ -0,0 +1,23 @@
+resource "digitalocean_loadbalancer" "ingress_load_balancer" {
+  name   = "${var.domain_name[0]}-lb"
+  region = var.do_region
+  size = "lb-medium"
+  algorithm = "round_robin"
+
+  forwarding_rule {
+    entry_port     = 80 
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+
+  }
+
+  lifecycle {
+      ignore_changes = [
+        forwarding_rule,
+    ]
+  }
+
+  vpc_uuid = digitalocean_vpc.cinema.id 
+}
