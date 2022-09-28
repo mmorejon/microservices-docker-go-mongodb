fe39aaa5ef6cd56dd3b565c4d7cabca9541511cb
Brian Adams <service@contrasting.org>
2022-09-28 10:43:07 -0500

    feat: readd istio

--- digitalocean/gateway.tf
+++ digitalocean/gateway.tf
@@ -1,33 +1,43 @@
 resource "kubernetes_manifest" "argocd-gateway" {
   provider = kubernetes.cinema
   manifest = {
-    "apiVersion" = "networking.istio.io/v1beta1"
-    "kind"       = "Gateway"
-    "metadata" = {
-      "name"      = "argocd"
-      "namespace" = "argocd"
+  "apiVersion" = "networking.istio.io/v1beta1"
+  "kind" = "Gateway"
+  "metadata" = {
+    "name" = "argocd"
+    "namespace" = "argocd"
+  }
+  "spec" = {
+    "selector" = {
+      "istio" = "ingressgateway"
     }
-    "spec" = {
-      "selector" = {
-        "istio" = "ingressgateway"
-      }
-      "servers" = [
-        {
-          "hosts" = [
-            "argocd.wayofthesys.org",
-          ]
-          "port" = {
-            "name"     = "https"
-            "number"   = 443
-            "protocol" = "HTTPS"
-          }
-          "tls" = {
-            "credentialName" = "argocd-secret"
-            "httpsRedirect"  = false
-            "mode"           = "SIMPLE"
-          }
-        },
-      ]
-    }
+    "servers" = [
+      {
+        "hosts" = [
+          "argocd.wayofthesys.org",
+        ]
+        "port" = {
+          "name" = "http"
+          "number" = 80
+          "protocol" = "HTTP"
+        }
+      },
+      {
+        "hosts" = [
+          "argocd.wayofthesys.org",
+        ]
+        "port" = {
+          "name" = "https"
+          "number" = 443
+          "protocol" = "HTTPS"
+        }
+        "tls" = {
+          "credentialName" = "argocd-secret"
+          "httpsRedirect" = false
+          "mode" = "SIMPLE"
+        }
+      },
+    ]
   }
 }
+
