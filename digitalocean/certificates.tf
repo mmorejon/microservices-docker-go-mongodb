fdee216bfe9e3e6df7776a6a83bf1a144cf350db
Brian Adams <service@contrasting.org>
2022-09-28 12:41:50 -0500

    feat: readd istio

--- digitalocean/certificates.tf
+++ digitalocean/certificates.tf
@@ -1,4 +1,27 @@
-resource "kubernetes_manifest" "certificate_resource" {
+resource "kubernetes_manifest" "certificate_argo_argo" {
+  provider = kubernetes.cinema
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "argocd-cert"
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "commonName" = "argocd.${var.domain_name[0]}"
+      "dnsNames" = [
+        "argocd.${var.domain_name[0]}",
+      ]
+      "issuerRef" = {
+        "kind" = "ClusterIssuer"
+        "name" = "zerossl"
+      }
+      "secretName" = "${replace(var.domain_name[0], ".", "-")}-tls"
+    }
+  }
+} 
+
+resource "kubernetes_manifest" "certificate_argo_istio" {
   provider = kubernetes.cinema
   manifest = {
     "apiVersion" = "cert-manager.io/v1"
