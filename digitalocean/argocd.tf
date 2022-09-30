dbe29ef8a52795dbbed1d64e339e007865b0d330
Brian Adams <service@contrasting.org>
2022-09-30 09:24:02 -0500

    fix: add admin password in argocd helm chart

---
+++ digitalocean/argocd.tf
@@ -0,0 +1,60 @@
+resource "kubernetes_service_account" "argocd_manager" {
+  metadata {
+    name      = "argocd-manager"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role" "argocd_manager" {
+  metadata {
+    name = "argocd-manager-role"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+
+  rule {
+    non_resource_urls = ["*"]
+    verbs             = ["*"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_manager" {
+  metadata {
+    name = "argocd-manager-role-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.argocd_manager.metadata.0.name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.argocd_manager.metadata.0.name
+    namespace = kubernetes_service_account.argocd_manager.metadata.0.namespace
+  }
+}
+
+data "kubernetes_secret" "argocd_manager" {
+  metadata {
+    name      = kubernetes_service_account.argocd_manager.default_secret_name
+    namespace = kubernetes_service_account.argocd_manager.metadata.0.namespace
+  }
+}
+
+resource "argocd_cluster" "gke" {
+  server = format("https://%s", digitalocean_kubernetes_cluster.cinema.endpoint)
+  name   = "do-cinema"
+
+  config {
+    bearer_token = data.kubernetes_secret.argocd_manager.data["token"]
+    tls_client_config {
+      ca_data      = base64decode(digitalocean_kubernetes_cluster.cinema.kube_config[0].cluster_ca_certificate)
+    }
+  }
+}
