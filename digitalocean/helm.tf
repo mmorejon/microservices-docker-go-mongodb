c472e4a90d8af7c09c420b4fb6f5a63e4ca05a43
Brian Adams <service@contrasting.org>
2022-09-30 16:03:50 -0500

    fix: Added oidc argocd secret var

--- digitalocean/helm.tf
+++ digitalocean/helm.tf
@@ -196,12 +196,7 @@ resource "helm_release" "argocd" {
 
   set_sensitive {
     name  = "server.config.dex.config"
-    value = "connectors ={\n"
-    config = {
-      "issuer"       = "https://accounts.google.com\n"
-      "clientID"     = "${var.argocd_oidc_client_id}\n"
-      "clientSecret" = "${var.argocd_oidc_client_secret}"
-    }
+    value = "connectors ={\n config = {\n \"issuer\"       = \"https://accounts.google.com\n\" \"clientID\"     = \"${var.argocd_oidc_client_id}\"\n \"clientSecret\" = \"${var.argocd_oidc_client_secret}\"}}"
   }
 }
 
