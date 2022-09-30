2a74bd2e99d1ac8f8fc662f07a4dc649761beb7d
Brian Adams <service@contrasting.org>
2022-09-30 13:54:36 -0500

    fix: add admin password in argocd helm chart

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -73,7 +73,8 @@ resource "argocd_cluster" "do-cinema" {
   name   = "do-cinema"
 
   config {
-    bearer_token = data.kubernetes_secret.argocd_manager.data["token"]
+    bearer_token = digitalocean_kubernetes_cluster.cinema.kube_config[0].token 
+    # data.kubernetes_secret.argocd_manager.data["token"]
     tls_client_config {
       ca_data = base64decode(digitalocean_kubernetes_cluster.cinema.kube_config[0].cluster_ca_certificate)
       cert_data = base64decode(digitalocean_kubernetes_cluster.cinema.kube_config[0].client_certificate)
