a14475e5bceed27d81664364bacdbe20edcd39ca
Brian Adams <service@contrasting.org>
2022-09-30 18:49:04 -0500

    fix: Added oidc argocd secret var

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -75,7 +75,7 @@ resource "argocd_cluster" "do-cinema" {
   config {
     bearer_token = data.kubernetes_secret.argocd_manager.data["token"]
     tls_client_config {
-      ca_data = base64decode(data.kubernetes_secret.argocd_manager.data["ca.crt"])
+      ca_data = data.kubernetes_secret.argocd_manager.data["ca.crt"]
       // cert_data = base64decode(digitalocean_kubernetes_cluster.cinema.kube_config[0].client_certificate)
       // key_data = base64decode(digitalocean_kubernetes_cluster.cinema.kube_config[0].client_key)
     }
