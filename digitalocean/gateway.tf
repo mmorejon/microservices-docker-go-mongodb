0b966032b0ce676b46457c20faf57f393085c7d8
Brian Adams <service@contrasting.org>
2022-10-03 01:41:37 -0500

    feat: add gateway and virtualservice for cinema

--- digitalocean/gateway.tf
+++ digitalocean/gateway.tf
@@ -84,7 +84,7 @@ resource "kubernetes_manifest" "cinema-gateway" {
             "protocol" = "HTTPS"
           }
           "tls" = {
-            "credentialName"    = "${replace(var.domain_name[0], ".", "-")}-tls"
+            "credentialName"    = "cinema-cert"
             "mode"              = "SIMPLE"
             "serverCertificate" = "/etc/istio/ingressgateway-certs/tls.crt"
             "privateKey"        = "/etc/istio/ingressgateway-certs/tls.key"
