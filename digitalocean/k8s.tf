be8c7be67672422a467387e2537167dcad8f549b
Brian Adams <service@contrasting.org>
2022-10-03 15:42:18 -0500

    feat: add load testing cluster

--- digitalocean/k8s.tf
+++ digitalocean/k8s.tf
@@ -13,3 +13,19 @@ resource "digitalocean_kubernetes_cluster" "cinema" {
   }
   vpc_uuid = digitalocean_vpc.cinema.id
 }
+
+resource "digitalocean_kubernetes_cluster" "loadtesting" {
+  name   = "loadtesting"
+  region = "nyc3"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "core"
+    size       = "s-4vcpu-8gb"
+    auto_scale = true
+    min_nodes  = 4
+    max_nodes  = 4 
+  }
+  vpc_uuid = digitalocean_vpc.loadtesting.id
+}
--- digitalocean/vpc.tf
+++ digitalocean/vpc.tf
@@ -4,4 +4,10 @@ resource "digitalocean_vpc" "cinema" {
   ip_range = "10.1.0.0/22" // 2046 max hosts
 }
 
+resource "digitalocean_vpc" "loadtesting" {
+  name     = "loadtesting-prod"
+  region   = "nyc3"
+  ip_range = "10.2.0.0/22" // 2046 max hosts
+}
+
 
