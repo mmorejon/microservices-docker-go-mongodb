cd7810bb464ab5be43687e711f277a179bc67195
Brian Adams <service@contrasting.org>
2022-10-03 01:07:32 -0500

    feat: add gateway and virtualservice for cinema

--- digitalocean/kubernetes_secrets.tf
+++ digitalocean/kubernetes_secrets.tf
@@ -49,6 +49,20 @@ resource "kubernetes_secret" "argocd-tls" {
   }
 }
 
+resource "kubernetes_secret" "wayofthesys-tls" {
+  provider   = kubernetes.cinema
+  depends_on = [digitalocean_kubernetes_cluster.cinema, helm_release.external-dns, kubernetes_namespace.cinema]
+  metadata {
+    name = "${replace(var.domain_name[0], ".", "-")}-tls"
+    namespace = "istio-system"
+  }
+  type = "tls"
+  data = {
+    "tls.crt" = tls_locally_signed_cert.cert.cert_pem
+    "tls.key" = tls_private_key.key.private_key_pem
+  }
+}
+
 resource "kubernetes_secret" "zerossl-eab-hmac-key" {
   provider   = kubernetes.cinema
   depends_on = [digitalocean_kubernetes_cluster.cinema]
