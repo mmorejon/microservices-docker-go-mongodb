fd6c5d3549fbe2e72886d9f4694d1c8b23dfd73f
Brian Adams <service@contrasting.org>
2022-10-02 23:55:52 -0500

    feat: add gateway and virtualservice for cinema

--- digitalocean/gateway.tf
+++ digitalocean/gateway.tf
@@ -45,3 +45,52 @@ resource "kubernetes_manifest" "argocd-gateway" {
     }
   }
 }
+
+resource "kubernetes_manifest" "cinema-gateway" {
+  depends_on = [helm_release.argocd]
+  provider = kubernetes.cinema
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "Gateway"
+    "metadata" = {
+      "name"      = "cinema"
+      "namespace" = "cinema"
+    }
+    "spec" = {
+      "selector" = {
+        "istio" = "ingressgateway"
+      }
+      "servers" = [
+        {
+          "hosts" = [
+            "${var.domain_name[0]}",
+          ]
+          "port" = {
+            "name"     = "http"
+            "number"   = 80
+            "protocol" = "HTTP"
+          }
+          "tls" = {
+            "httpsRedirect" = false
+          }
+        },
+        {
+          "hosts" = [
+            "${var.domain_name[0]}",
+          ]
+          "port" = {
+            "name"     = "https"
+            "number"   = 443
+            "protocol" = "HTTPS"
+          }
+          "tls" = {
+            "credentialName"    = "${replace(var.domain_name[0], ".", "-")}-tls"
+            "mode"              = "SIMPLE"
+            "serverCertificate" = "/etc/istio/ingressgateway-certs/tls.crt"
+            "privateKey"        = "/etc/istio/ingressgateway-certs/tls.key"
+          }
+        },
+      ]
+    }
+  }
+}
--- digitalocean/virtualservice.tf
+++ digitalocean/virtualservice.tf
@@ -1,40 +1,81 @@
-resource "kubernetes_manifest" "virtualservice_resource" {
+resource "kubernetes_manifest" "argocd_virtualservice" {
   provider = kubernetes.cinema
   manifest = {
-  "apiVersion" = "networking.istio.io/v1beta1"
-  "kind" = "VirtualService"
-  "metadata" = {
-    "name" = "argocd"
-    "namespace" = "argocd"
-  }
-  "spec" = {
-    "gateways" = [
-      "argocd",
-    ]
-    "hosts" = [
-      "argocd.${var.domain_name[0]}",
-    ]
-    "http" = [
-      {
-        "match" = [
-          {
-            "uri" = {
-              "prefix" = "/"
-            }
-          },
-        ]
-        "route" = [
-          {
-            "destination" = {
-              "host" = "argocd-server"
-              "port" = {
-                "number" = 80
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "VirtualService"
+    "metadata" = {
+      "name"      = "argocd"
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "gateways" = [
+        "argocd",
+      ]
+      "hosts" = [
+        "argocd.${var.domain_name[0]}",
+      ]
+      "http" = [
+        {
+          "match" = [
+            {
+              "uri" = {
+                "prefix" = "/"
               }
-            }
-          },
-        ]
-      },
-    ]
+            },
+          ]
+          "route" = [
+            {
+              "destination" = {
+                "host" = "argocd-server"
+                "port" = {
+                  "number" = 80
+                }
+              }
+            },
+          ]
+        },
+      ]
+    }
   }
 }
+
+resource "kubernetes_manifest" "cinema_virtualservice" {
+  provider = kubernetes.cinema
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "VirtualService"
+    "metadata" = {
+      "name"      = "cinema"
+      "namespace" = "cinema"
+    }
+    "spec" = {
+      "gateways" = [
+        "argocd",
+      ]
+      "hosts" = [
+        var.domain_name[0],
+      ]
+      "http" = [
+        {
+          "match" = [
+            {
+              "uri" = {
+                "prefix" = "/"
+              }
+            },
+          ]
+          "route" = [
+            {
+              "destination" = {
+                "host" = "cinema-website"
+                "port" = {
+                  "number" = 80
+                }
+              }
+            },
+          ]
+        },
+      ]
+    }
+  }
 }
